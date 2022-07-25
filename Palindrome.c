1aee07fc6116e7703546aab1a12887d407b9f22b
sunneah <109723325+sunneah@users.noreply.github.com>
2022-07-25 12:35:00 +0530

    Update Palindrome.c

--- Palindrome.c
+++ Palindrome.c
@@ -1,5 +1,27 @@
 #include<stdio.h>
+#include<string.h>
 
 void main()
 {
+  char str[20]; 
+  int l,k,i,f=0;
+  printf("Enter a string:");
+  gets(str);
+  l=strlen(str);
+  k=l;
+  for(i=0;i<=l/2;i++)
+  {
+    if(str[i]!=str[k])
+    {f=1;
+     break();
+    }
+    k--;
+  }
+  
+  if(f==0)
+    printf("string is a palindrome");
+  else
+    printf("String is not a palindrome");
+   
+  
 }
